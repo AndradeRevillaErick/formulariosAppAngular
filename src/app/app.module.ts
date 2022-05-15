a616a3828b098d10665443fdfee9533532279e19
Erick Andrade <erick_andradhe@hotmail.com>
2022-05-15 13:42:00 -0500

    fix: fix error basicos component was declared in two places

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { SharedModule } from './shared/shared.module';
+// import { BasicosComponent } from './reactive/basicos/basicos.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // BasicosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
